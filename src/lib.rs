62eb056fc20d3fd19e1f87bda28958aeaae38090
Pietro Atzeni <pietrino.atzeni@gmail.com>
2022-03-18 18:53:12 +0100

    Refactored code

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,5 @@
-use std::net::TcpListener;
+mod routes;
+mod configuration;
+mod startup;
 
-use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().finish()
-}
-
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
-
-    Ok(server)
-}
+pub use crate::startup::run;
